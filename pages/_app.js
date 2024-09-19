a5aa3114881562d4e2e01405f3496526815854e6
William Njau <willymboche369@gmail.com>
2024-09-18 20:43:26 -0700

    run

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import "@/styles/globals.css";
+// bring in bootstrap css file with responsive css media queries
+import '../styles/bootstrap.min.css';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// define our next.js' master MyApp
+function MyApp( { Component, pageProps } ) {
+  return (
+    <Component {...pageProps} />
+  );
 }
+
+export default MyApp;
