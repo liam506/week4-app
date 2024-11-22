42ed0dc345ccf056a2ce61f212d21fd917c3b13b
William Njau <willymboche369@gmail.com>
2024-11-21 22:27:12 -0800

    second

--- pages/[id].js
+++ pages/[id].js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Layout from '../components/layout';
 import { getAllIds, getData } from '../lib/data';
 
@@ -15,7 +16,7 @@ export async function getStaticProps( { params } ) {
 // define a getStaticPaths() function to tell next.js all valid URLs: 1,2,3,4 
 // - this name is defined by next.js
 export async function getStaticPaths() {
-  const paths = getAllIds();
+  const paths = await getAllIds();
   return {
     paths,
     fallback: false
@@ -28,10 +29,9 @@ export default function Entry( { itemData } ) {
     <Layout>
       <article className="card col-6">
         <div className="card-body">
-          <h5 className="card-title">{itemData.name}</h5>
-          <h6 className="card-subtitle mb-2 text-body-secondary">{itemData.phone}</h6>
-          <p className="card-text">{itemData.birthdate}</p>
-          <a href="#" className="card-link">{itemData.email}</a>
+          <h5 className="card-title">{itemData.post_title}</h5>
+          <h6 className="card-subtitle mb-2 text-body-secondary" >{itemData.user_login}</h6>
+          <div className= "card-text" dangerouslySetInnerHTML={{__html: itemData.post_content}}></div>  
         </div>
       </article>
     </Layout>
